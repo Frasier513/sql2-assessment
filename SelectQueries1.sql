1161703acf7ad3357a677990029adfc0d2ecc52f
Frasier <53498848+Frasier513@users.noreply.github.com>
2026-05-08 17:13:02 -0400

    Add files via upload

---
+++ SelectQueries1.sql
@@ -0,0 +1,22 @@
+/*use food_order_app database */
+use food_order_app;
+
+/* 1. Display all distinct cuisine names. */
+
+/* 2. Display description of 'Cake' category. */
+
+/* 3. Select all restaurants where restaurant names ends with 'Junction'. */
+
+/* 4. Display details of all vegeterian fooditems. */
+
+/* 5. Retrieve all menu items with food items that have a price equal to or less than $10. */
+
+/* 6.Retrieve cartId having the maximum number of food items. */
+
+/* 7. Retrieve all the shipping details where emailId contains 'doe'. */
+
+/* 8. Retrieve the order with the highest price. */
+ 
+/* 9. Retrieve the most recently updated order. */
+
+/* 10. How many total units of food items were purchased in orderId 1? */
---
+++ SelectQueries2.sql
@@ -0,0 +1,15 @@
+/* use food_order_app database */
+use food_order_app;
+
+/* 1. Retrieve number of fooditems for each cuisine */
+
+/* 2. Retrieve category names in the order of highest to lowest no. of fooditems */
+
+/* 3. Retrieve cuisine name as CuisineName, fooditem name as name FoodItemName 
+and description as Description of all the fooditems belonging to 'Italian' cuisine */
+
+/* 4. Retrieve details of food items, including their name, vegetarian status,
+ and cuisine name, for only those food items that are vegetarian. */
+
+/* 5. Retrieve the details of users who have items in their cart 
+along with the total order amount */
